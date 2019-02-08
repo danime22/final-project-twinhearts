3332e97c54631efa7e6ac16651fae2a057ded661
Adreena Howard <adreena.howard@yahoo.com>
2019-02-07 19:48:05 -0600

    added passport

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const messageRoutes = require("./messages");
+const authRoutes = require('./auth');
 
 
 router.use("/users", userRoutes);
 // router.use("/login", userRoutes);
 
 router.use("/messages", messageRoutes);
+
+router.use("/auth", authRoutes);
 
 
 module.exports = router;
